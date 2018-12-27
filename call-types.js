f92e2d96b6c7f97cf96482257a3815c7469a47d4
Khurram Qureshi <khurram@codility.co>
2018-12-27 16:08:48 +0500

    deleted extra types of call

--- call-types.js
+++ call-types.js
@@ -3,6 +3,6 @@ module.exports = {
     INCOMING : android.provider.CallLog.Calls.INCOMING_TYPE,
     MISSED   : android.provider.CallLog.Calls.MISSED_TYPE,
     // REJECTED : android.provider.CallLog.Calls.REJECTED_TYPE,
-    BLOCKED  : android.provider.CallLog.Calls.BLOCKED_TYPE,
-    VOICEMAIL: android.provider.CallLog.Calls.VOICEMAIL_TYPE
+    // BLOCKED  : android.provider.CallLog.Calls.BLOCKED_TYPE,
+    // VOICEMAIL: android.provider.CallLog.Calls.VOICEMAIL_TYPE
 };
